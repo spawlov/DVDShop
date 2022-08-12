50b7c1ed73a493a5d024d29322fbe399e4cd3b51
spawlov <spawlov@gmail.com>
2022-08-12 19:52:47 +0700

    changed views.py, apps.py, template

--- DVDShop/storedvd/apps.py
+++ DVDShop/storedvd/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class StoredvdConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'storedvd'
+    verbose_name = 'Магазин DVD'
--- DVDShop/storedvd/views.py
+++ DVDShop/storedvd/views.py
@@ -2,12 +2,20 @@ from django.shortcuts import render
 from django.template import context
 from django.views.generic import ListView
 
-from .models import Product
+from .models import Product, Section
 
 
 class Index(ListView):
     model = Product
     template_name = 'index.html'
-    ...
+    
+    def get_context_data(self, *, object_list=None, **kwargs):
+        contexts = super(Index, self).get_context_data(**kwargs)
+        contexts['sections'] = Section.objects.all()
+        return contexts
+
+
 # def index(request):
-#     return render(request, 'index.html')
+#     sections = Section.objects.all()
+#     context = {'sections': sections}
+#     return render(request, 'index.html', context=context)
