d6033fa527adb2f0400008c8d5028d9a9c495286
sergey <spawlov@gmail.com>
2022-08-30 20:25:58 +0700

    changed index.html, views.py = added sorted to index page; changed urls.py and views.py = changed at classes views to def views

--- DVDShop/storedvd/urls.py
+++ DVDShop/storedvd/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from . import views
-from .views import Index
+from .views import index
+# from .views import Index
 
 app_name = 'storedvd'
 urlpatterns = [
-    # path('', views.index, name='index'),
-    path('', Index.as_view(), name='index'),
+    path('', index, name='index'),
 ]
--- DVDShop/storedvd/views.py
+++ DVDShop/storedvd/views.py
@@ -5,19 +5,22 @@ from django.views.generic import ListView
 from .models import Product, Section
 
 
-class Index(ListView):
-    # model = Product
-    queryset = Product.objects.all()[:8]
-    template_name = 'index.html'
-    context_object_name = 'products'
-    
-    def get_context_data(self, *, object_list=None, **kwargs):
-        contexts = super(Index, self).get_context_data(**kwargs)
-        contexts['sections'] = Section.objects.order_by('title').all()
-        return contexts
+def sorted_title_or_price(request):
+    order_by = '-date'
+    if all([request.GET.__contains__('sort'), request.GET.__contains__('up')]):
+        if all([
+            any([request.GET['sort'] == 'title', request.GET['sort'] == 'price']),
+            any([request.GET['up'] == '0', request.GET['up'] == '1']),
+        ]):
+            order_by = f'{"" if request.GET["up"] == "1" else "-"}{request.GET["sort"]}'
+    return order_by
 
 
-# def index(request):
-#     sections = Section.objects.all()
-#     context = {'sections': sections}
-#     return render(request, 'index.html', context=context)
+def index(request):
+    sections = Section.objects.order_by('title').all()
+    products = Product.objects.order_by(sorted_title_or_price(request)).all()[:8]
+    context = {
+        'sections': sections,
+        'products': products,
+    }
+    return render(request, 'index.html', context=context)
